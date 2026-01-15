2659dc98bc849f754d00c52614e71338a86cf8c8
12785@holbertonstudents.com <you@example.com>
2026-01-15 20:44:34 +0100

    Initial commit

--- ES6_basic/2-main.js
+++ ES6_basic/2-main.js
@@ -1,5 +1,5 @@
 import getNeighborhoodsList from './2-arrow.js';
-
+/*eslint-disable*/
 const neighborhoodsList = new getNeighborhoodsList();
 const res = neighborhoodsList.addNeighborhood('Noe Valley');
 console.log(res);

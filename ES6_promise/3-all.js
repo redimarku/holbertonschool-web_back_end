d9db4ad9a9a27cf946f236298f2d14ecc5778b62
12785@holbertonstudents.com <you@example.com>
2026-01-21 22:59:24 +0100

    Initial commit

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,6 +1,14 @@
-import { uploadPhoto ,createUser } from "./utils.js";
+/*eslint-disable*/
+import { uploadPhoto, createUser } from './utils.js';
 
-function handleProfileSignUp(){
-    return Promise.all([uploadPhoto(), createUser())]
-    .then()
+function handleProfileSignup() {
+	return Promise.all([uploadPhoto(), createUser()])
+		.then((Response) => {
+			console.log(
+				`${Response[0].body} ${Response[1].firstName} ${Response[1].lastName}`
+			);
+		})
+		.catch(() => console.log('Signup system offline'));
 }
+
+export default handleProfileSignup;

a68c5698526988f2f5b34db243fbaca5cc9849a9
12785@holbertonstudents.com <you@example.com>
2026-01-25 11:34:02 +0100

    Initial commit

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,26 @@
+/*eslint-disable*/
+export default class Currency{
+    constructor(code, name){
+        this._code = code;
+        this._name = name;
+    }
+
+    get code(){
+        return this._code;
+    }
+
+    get name(){
+        return this._name
+    }
+
+    set name(name){
+        this._name = name;
+    }
+    set name(code){
+        this._code = code;
+    }
+
+    displayFullCurrency(){
+        return(`${this._name} (${this._code})`);
+    }
+}

8f6ae8b955dd8812eb37e328f8848832902c68d0
12785@holbertonstudents.com <you@example.com>
2026-01-27 11:15:30 +0100

    Initial commit

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -30,7 +30,7 @@ export class StudentHolberton {
   }
 
   get fullStudentDescription() {
-    return `${this._firstName} ${this._lastName} - ${this._holbertonClass._year} ${this._holbertonClass._location}`;
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass._year} - ${this._holbertonClass._location}`;
   }
 }
 

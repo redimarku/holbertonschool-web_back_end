a84ce1a3f6a1039be74fd73a3f3c62b09decb1eb
12785@holbertonstudents.com <you@example.com>
2026-01-27 13:31:40 +0100

    Initial commit

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,23 @@
+/*eslint-disable*/
+export default class Car{
+    constructor(brand, motor, color){
+   
+            this._brand = brand;
+      
+
+        
+            this._motor = motor;
+       
+
+    
+            this._color = color;
+        
+    }
+ 
+
+    cloneCar() {
+const Species = this.constructor[Symbol.species];
+return new Species();
+}
+
+}

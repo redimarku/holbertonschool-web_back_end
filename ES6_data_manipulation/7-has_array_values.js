16b3449d535a461b75529de016d7c67738e52738
12785@holbertonstudents.com <you@example.com>
2026-01-30 22:12:50 +0100

    Initial commit

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,3 +1,3 @@
-export default function hasValuesFromArray(set, array){
-  return array.every((el)=>{set.has(el)});
+export default function hasValuesFromArray(set, array) {
+  return array.every((el) => (set.has(el)));
 }
--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,12 @@
-export default function cleanSet(set, string){
-  if(!string || string.length === 0) {
+export default function cleanSet(set, string) {
+  if (!string || string.length === 0) {
     return '';
-    }
-    const values = [];
-  for(const x of set.keys()) {
-    if(x.includes(string)){
-      values.push(x.replace(string,''));
+  }
+  const values = [];
+  for (const x of set.keys()) {
+    if (x.includes(string)) {
+      values.push(x.replace(string, ''));
     }
   }
-  return values.join("-");
-  
+  return values.join('-');
 }

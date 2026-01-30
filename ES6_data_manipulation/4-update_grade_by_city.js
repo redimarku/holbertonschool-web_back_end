e86ea917c803ad44ab8fff7ff6e3213770c4ecd2
12785@holbertonstudents.com <you@example.com>
2026-01-30 22:05:37 +0100

    Initial commit

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,20 @@
-export default function updateStudentGradeByCity(students, city, newGrades){
-    const studentByCity = students.filter((student)=>{student.location === city});
+/*Eslint-disable*/
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  const studentByCity = students.filter(
+    (student) => student.location === city
+  );
 
-    const result = studentByCity.map((el)=>{
-        const grades = newGrades.find((newGrade)=>{return newGrade.studentId === el.id});
-        const grade = grades.grade ? grades.grade :'N/A';
-        return{
-            ...el,
-            grade
-        }
-    })
-} 
+  const result = studentByCity.map((el) => {
+    const grades = newGrades.find(
+      (newGrade) => newGrade.studentId === el.id
+    );
+    const grade = grades ? grades.grade : 'N/A';
+
+    return {
+      ...el,
+      grade,
+    };
+  });
+
+  return result;
+}

0007c2d31fdc1524e4fcfc9cdabe112452fa1f8e
12785@holbertonstudents.com <you@example.com>
2026-02-28 19:32:30 +0100

    Initial commit

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,5 @@
--- This file lists all bands with Glam rock style ranked by longevity
 SELECT band_name,
-       COALESCE(split, 2023) - formed + 1 AS lifespan
+       COALESCE(split, 2023) - formed AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;

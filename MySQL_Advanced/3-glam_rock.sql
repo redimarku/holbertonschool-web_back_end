dd8f9f201cf99463ab8f95c7c684dd29bc419b46
12785@holbertonstudents.com <you@example.com>
2026-02-28 19:05:32 +0100

    Initial commit

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
--- script to list all databases
+-- lists all bands with Glam rock as their main style, ranked by longevity
+
 SELECT band_name,
-       -- Nëse banda ka vit split, përdoret ai; në të kundërt përdoret 2023
-       IFNULL(split, 2023) - formed + 1 AS lifespan
+       IFNULL(split, 2024) - formed + 1 AS lifespan
 FROM metal_bands
-WHERE style LIKE "%Glam rock%"
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;

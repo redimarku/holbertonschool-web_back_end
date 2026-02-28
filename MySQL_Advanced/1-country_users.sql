d3588e86724abb93989a20a80937f20071cab0ef
12785@holbertonstudents.com <you@example.com>
2026-02-28 18:54:26 +0100

    Initial commit

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,7 @@
 -- script to list all databases
 CREATE TABLE IF NOT EXISTS users (
-        id int NOT NULL AUTO_INCREMENT,
+        id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
         email varchar(255) NOT NULL UNIQUE,
         name varchar(255),
         country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
-        PRIMARY KEY (id)
 );

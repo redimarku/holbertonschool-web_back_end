5c9b2c7ac789a56c1ab4263f79c06a9aa0899622
12785@holbertonstudents.com <you@example.com>
2026-02-28 21:18:52 +0100

    Initial commit

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,7 @@
 -- script to list all databases
-SELECT band_name,
-       COALESCE(split, 2023) - formed + 1 AS lifespan
+SELECT
+    band_name,
+    COALESCE(split, 2024) - formed AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;

4f57f469be822fbdcc541d99a074a59dfdec5776
12785@holbertonstudents.com <you@example.com>
2026-02-28 19:04:34 +0100

    Initial commit

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,7 @@
 -- script to list all databases
 SELECT band_name,
+       -- Nëse banda ka vit split, përdoret ai; në të kundërt përdoret 2023
        IFNULL(split, 2023) - formed + 1 AS lifespan
 FROM metal_bands
-WHERE style LIKE '%Glam rock%'
+WHERE style LIKE "%Glam rock%"
 ORDER BY lifespan DESC;
